1d3330a67d296ffaf488b74c2dce62de6cd14e70
Ikeda Harket <sebaaignacio111@gmail.com>
2021-09-29 22:24:47 -0300

    cors agregado

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -19,6 +20,7 @@ class Server{
 
     middlewares(){
         this.app.use( express.static( path.resolve( __dirname,'../public' )));
+        this.app.use(cors());
     }
     configurarSockets(){
         new Sockets(this.io);
